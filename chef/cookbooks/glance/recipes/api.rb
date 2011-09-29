0dfc8afbbc3fdaaa49b7192d57602fede5588ad8
Justin Shepherd <jshepher@rackspace.com>
2011-09-29 14:52:12 -0500

    removed glance.conf template from glance::common, added api and registry config templates

--- chef/cookbooks/glance/recipes/api.rb
+++ chef/cookbooks/glance/recipes/api.rb
@@ -6,6 +6,13 @@
 
 include_recipe "#{@cookbook_name}::common"
 
+template "/etc/glance/glance-api.conf" do
+  source "glance-api.conf.erb"
+  owner node[:glance][:user]
+  group "root"
+  mode 0644
+end
+
 glance_service "api"
 
 node[:glance][:monitor][:svcs] <<["glance-api"]
--- chef/cookbooks/glance/recipes/registry.rb
+++ chef/cookbooks/glance/recipes/registry.rb
@@ -6,6 +6,13 @@
 
 include_recipe "#{@cookbook_name}::common"
 
+template "/etc/glance/glance-registry.conf" do
+  source "glance-registry.conf.erb"
+  owner node[:glance][:user]
+  group "root"
+  mode 0644
+end
+
 glance_service "registry"
 
 node[:glance][:monitor][:svcs] << ["glance-registry"]

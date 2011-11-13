f89c0806d5cc9918f658a82062562ead50f886a6
Greg Althaus <galthaus@austin.rr.com>
2011-11-12 23:00:02 -0600

    Add mysql to the dependency list

--- chef/cookbooks/glance/metadata.rb
+++ chef/cookbooks/glance/metadata.rb
@@ -5,4 +5,5 @@ description      "Installs/Configures Glance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2"
 depends          "keystone"
+depends          "mysql"
 

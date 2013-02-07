097f4e689986a32c7d73fab655606f8ee61774a3
Evgeniy Shurmin <eshurmin@gmai.com>
2013-02-07 16:55:05 +0400

    DCB-64 fix

--- chef/cookbooks/glance/metadata.rb
+++ chef/cookbooks/glance/metadata.rb
@@ -5,7 +5,8 @@ description      "Installs/Configures Glance"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2"
 depends          "keystone"
+# needed for correctly apply this recipe
+depends          "barclamp"
 depends          "mysql"
 depends          "openssl"
 depends          "nagios"
-

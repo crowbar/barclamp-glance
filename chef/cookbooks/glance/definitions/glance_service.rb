a56b3c4ed7b4ab5e55a92c569c6bcdf1c5ddde2b
Maxim Datskovskiy <mdatskovsky@mirantis.com>
2013-08-15 16:12:20 +0300

    fix service for rhel

--- chef/cookbooks/glance/definitions/glance_service.rb
+++ chef/cookbooks/glance/definitions/glance_service.rb
@@ -1,7 +1,7 @@
 define :glance_service do
 
   glance_name="glance-#{params[:name]}"
-  glance_name="openstack-glance-#{params[:name]}" if node.platform == "suse"
+  glance_name="openstack-glance-#{params[:name]}" if node.platform?(%w{centos redhat suse})
   short_name="#{params[:name]}"
 
   service glance_name do

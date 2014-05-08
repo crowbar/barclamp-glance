bcf5bd2fdd4efe5070c6711097f43ca5035ba21f
Thomas Boerger <tboerger@suse.de>
2014-05-08 10:39:45 +0200

    Reafctored most parts and did some unification

--- crowbar_framework/lib/crowbar/registry/glance.rb
+++ crowbar_framework/lib/crowbar/registry/glance.rb
@@ -7,7 +7,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,35 +16,10 @@
 # limitations under the License.
 #
 
-module GlanceBarclampHelper
-  def api_protocols_for_glance(selected)
-    options_for_select(
-      [
-        ["HTTP","http"],
-        ["HTTPS", "https"]
-      ],
-      selected.to_s
-    )
-  end
+module Crowbar
+  module Registry
+    class Glance < Base
 
-  def notifier_strategies_for_glance(selected)
-    options_for_select(
-      [
-        ["Noop","noop"],
-        ["RabbitMQ", "rabbit"]
-      ],
-      selected.to_s
-    )
-  end
-
-  def default_stores_for_glance(selected)
-    options_for_select(
-      [
-        ["File","file"],
-        ["Swift", "swift"],
-        ["Rados", "rbd"]
-      ],
-      selected.to_s
-    )
+    end
   end
 end

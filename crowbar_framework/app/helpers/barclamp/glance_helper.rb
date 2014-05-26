927aee4a4788a7fe3bd534508832980f66541d7b
Thomas Boerger <tboerger@suse.de>
2014-05-26 14:48:00 +0200

    Moved helper into barclamp namespace

---
+++ crowbar_framework/app/helpers/barclamp/glance_helper.rb
@@ -0,0 +1,51 @@
+#
+# Copyright 2011-2013, Dell
+# Copyright 2013-2014, SUSE LINUX Products GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Barclamp
+  module GlanceHelper
+    def api_protocols_for_glance(selected)
+      options_for_select(
+        [
+          ["HTTP", "http"],
+          ["HTTPS", "https"]
+        ],
+        selected.to_s
+      )
+    end
+
+    def notifier_strategies_for_glance(selected)
+      options_for_select(
+        [
+          ["Noop", "noop"],
+          ["RabbitMQ", "rabbit"]
+        ],
+        selected.to_s
+      )
+    end
+
+    def default_stores_for_glance(selected)
+      options_for_select(
+        [
+          ["File", "file"],
+          ["Swift", "swift"],
+          ["Rados", "rbd"]
+        ],
+        selected.to_s
+      )
+    end
+  end
+end

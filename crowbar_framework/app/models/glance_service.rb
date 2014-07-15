f411949c6eeb4e32af7583c482d88d66ad2b4040
Vincent Untz <vuntz@suse.com>
2014-07-15 11:03:25 +0200

    Fail if rabbitmq proposal cannot be found on creation
    RabbitMQ is now a hard dependency of glance, since 29f4cc0a.

--- crowbar_framework/app/models/glance_service.rb
+++ crowbar_framework/app/models/glance_service.rb
@@ -65,7 +65,7 @@ class GlanceService < PacemakerServiceObject
 
     base["attributes"][@bc_name]["git_instance"] = find_dep_proposal("git", true)
     base["attributes"][@bc_name]["database_instance"] = find_dep_proposal("database")
-    base["attributes"][@bc_name]["rabbitmq_instance"] = find_dep_proposal("rabbitmq", true)
+    base["attributes"][@bc_name]["rabbitmq_instance"] = find_dep_proposal("rabbitmq")
     base["attributes"][@bc_name]["keystone_instance"] = find_dep_proposal("keystone")
 
     base["attributes"]["glance"]["service_password"] = random_password
